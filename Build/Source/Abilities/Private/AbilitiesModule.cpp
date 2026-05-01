d8d00f4692e0cfdd8a15841a25ea7c64490aeaa9
muit <muit@piperift.com>
2026-05-01 13:21:32 +0200

    Removed build data

--- Build/Source/Abilities/Private/AbilitiesModule.cpp
+++
@@ -1,8 +0,0 @@
-
-// Copyright 2020 Splash Damage, Ltd. - All Rights Reserved.
-
-#include "AbilitiesModule.h"
-
-DEFINE_LOG_CATEGORY(LogAbilities)
-
-IMPLEMENT_MODULE(FAbilitiesModule, Abilities);
--- Build/Source/Test/Private/AbilitiesTestModule.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2020 Splash Damage, Ltd. - All Rights Reserved.
-
-#include "AbilitiesTestModule.h"
-
-IMPLEMENT_MODULE(FAbilitiesTestModule, AbilitiesTest);
